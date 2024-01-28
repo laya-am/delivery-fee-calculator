42836e6c6f0bb1f5fc676613fb0295ccae6c13d1
Laya Aminosharei <laya.aminosharei@gmail.com>
2024-01-28 23:28:26 +0100

    deactivate dates before today in datepicker

--- src/components/DateInput/index.tsx
+++ src/components/DateInput/index.tsx
@@ -1,10 +1,12 @@
 import { FormControl, FormLabel, Input } from "@chakra-ui/react"
 
 export default function DateInput() {
+  const today = new Date().toISOString().split('T')[0];
+  
   return (
     <FormControl isRequired>
         <FormLabel htmlFor="date">Date</FormLabel>
-        <Input type="date" id="date" name="date" />
+        <Input type="date" id="date" name="date" min={today} />
     </FormControl>
   )
 }
