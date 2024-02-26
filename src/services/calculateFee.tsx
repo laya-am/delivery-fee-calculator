e970cc8841b6d6e02763e190e93e27c7915345f3
Laya Aminosharei <laya.aminosharei@gmail.com>
2024-02-26 12:09:57 +0100

    add variables to calculateFee func to make it more readable

--- src/services/calculateFee.tsx
+++ src/services/calculateFee.tsx
@@ -4,24 +4,45 @@ export interface CalculationData {
   numOfItems: number;
   orderTime: string;
 }
+const CART_VALUE_THRESHOLD = 10;
+const DISTANCE_THRESHOLD = 1000;
+const DISTANCE_SURCHARGE_UNIT = 500;
+const ITEMS_SURCHARGE_THRESHOLD = 4;
+const ITEMS_SURCHARGE_RATE = 0.5;
+const BULK_ITEMS_THRESHOLD = 12;
+const BULKFEE_SURCHARGE = 1.2;
+const SURCHARGE_RATE = 1.2;
+const MIN_FEE = 2;
+const MAX_FEE = 15;
+const FREE_DELIVERY_THRESHOLD = 200;
+const HIGH_LOAD_DAY = 5;
+const RUSH_HOUR_START = 15;
+const RUSH_HOUR_END = 18;
 
 export default function calculateFee(data: CalculationData): number {
   const { cartValue, distance, numOfItems, orderTime } = data;
   let newFee: number;
 
-  const cartSurcharge: number = cartValue < 10 ? 10 - cartValue : 0;
+  const cartSurcharge: number =
+    cartValue < CART_VALUE_THRESHOLD ? CART_VALUE_THRESHOLD - cartValue : 0;
   const deliveryFee: number =
-    2 + (distance > 1000 ? Math.ceil((distance - 1000) / 500) : 0);
+    MIN_FEE +
+    (distance > DISTANCE_THRESHOLD
+      ? Math.ceil((distance - DISTANCE_THRESHOLD) / DISTANCE_SURCHARGE_UNIT)
+      : 0);
   const numOfItemsSurcharge: number =
-    numOfItems > 4 ? (numOfItems - 4) * 0.5 : 0;
-  const bulkFee: number = numOfItems > 12 ? 1.2 : 0;
+    numOfItems > ITEMS_SURCHARGE_THRESHOLD
+      ? (numOfItems - ITEMS_SURCHARGE_THRESHOLD) * ITEMS_SURCHARGE_RATE
+      : 0;
+  const bulkFee: number =
+    numOfItems > BULK_ITEMS_THRESHOLD ? BULKFEE_SURCHARGE : 0;
 
   newFee =
-    cartValue >= 200
+    cartValue >= FREE_DELIVERY_THRESHOLD
       ? 0
       : Math.min(
           cartSurcharge + deliveryFee + bulkFee + numOfItemsSurcharge,
-          15,
+          MAX_FEE
         );
 
   const date = orderTime.slice(0, 10);
@@ -30,8 +51,8 @@ export default function calculateFee(data: CalculationData): number {
   const hour = +time.slice(0, 2);
   const day = new Date(date).getDay();
 
-  day === 5 && hour >= 15 && hour <= 18
-    ? (newFee = Math.min(newFee * 1.2, 15))
+  day === HIGH_LOAD_DAY && hour >= RUSH_HOUR_START && hour <= RUSH_HOUR_END
+    ? (newFee = Math.min(newFee * SURCHARGE_RATE, MAX_FEE))
     : null;
 
   return newFee;
